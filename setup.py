2231f030a02edad2529540a80ba398d80de62527
Guan Xipeng <guanxipeng@4paradigm.com>
2018-12-04 17:46:06 +0800

    v0.2.4

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ for line in open('requirements.txt', 'r'):
 
 setup(
     name='olo',
-    version='0.2.3',
+    version='0.2.4',
     keywords=('ORM', 'olo', 'cache', 'sqlstore'),
     description='ORM with intelligent and elegant cache manager',
     url='https://github.com/yetone/olo',
