ce97a8a6bb2fb284b4fd6c5a1606a878703e0d62
yetone <yetoneful@gmail.com>
2020-01-02 18:12:48 +0800

    debug

--- olo/logger.py
+++ olo/logger.py
@@ -6,4 +6,5 @@ if not logger.handlers:
     fmt = logging.Formatter("%(asctime)s %(filename)s %(funcName)s %(lineno)s \
         %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
     rht.setFormatter(fmt)
+    logger.addHandler(rht)
 logger.setLevel(logging.INFO)
